4fd017122e502285e53988f4044c3413fc86f8fa
MichaelSani05 <msmichaelsani@gmail.com>
2025-01-28 12:52:49 +0100

    V-0.0.5

--- src/app/pages/house/house.component.ts
+++ src/app/pages/house/house.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HouseService } from '../../services/house.service';
 import { BookingComponent } from '../../booking/booking.component';
+import { HousesComponent } from "../../components/houses/houses.component";
 
 @Component({
   selector: 'app-house',
   standalone: true,
-  imports: [BookingComponent],
+  imports: [BookingComponent, HousesComponent],
   templateUrl: './house.component.html',
   styleUrl: './house.component.css'
 })
@@ -14,6 +15,7 @@ export class HouseComponent {
   private route = inject(ActivatedRoute);
   productId!: number;
   house! : any[10]
+  houses! : any[]
 
   constructor(private houseService : HouseService) {}
 
@@ -22,8 +24,39 @@ export class HouseComponent {
       this.productId = parseInt(params.get('id')!);
       if (this.productId) {
         this.house = this.houseService.getHouseById(this.productId);
+        this.houses = this.houseService.filterHousesByCategory(this.house.category);
+        this.houses = this.houses.filter((house) => house.id !== this.house.id);
       }
     });
   }
 
+  isSticky: boolean = true;
+
+  ngAfterViewInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    this.checkScroll();
+  }
+
+  checkScroll(): void {
+    const houseSection = document.querySelector('.house-section');
+    const houseContent = document.querySelector('.house-content');
+    const housePrice = document.querySelector('.house-price');
+
+    if (houseSection && houseContent && housePrice) {
+      const sectionBottom = houseSection.getBoundingClientRect().bottom;
+      const viewportHeight = window.innerHeight;
+
+      if (sectionBottom <= viewportHeight) {
+        this.isSticky = false;
+        housePrice.classList.remove('sticky');
+      } else {
+        this.isSticky = true;
+        housePrice.classList.add('sticky');
+      }
+    }
+  }
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { HousesComponent } from '../../components/houses/houses.component';
 import { HouseService } from '../../services/house.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HousesComponent],
+  imports: [HousesComponent, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
   houses : any[] = [];
+  category : string = ''
   constructor(private houseService: HouseService){}
 
   ngOnInit(): void {
@@ -20,8 +22,10 @@ export class MainComponent {
   changeCategory(category : string){
     if (category == '') {
       this.houses = this.houseService.getHouses();
+      this.category = category;
     } else{
       this.houses = this.houseService.filterHousesByCategory(category);
+      this.category = category;
     }
   }
 
