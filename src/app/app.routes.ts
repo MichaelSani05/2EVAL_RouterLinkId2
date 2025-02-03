f040ed9184716341d44646a7cfd3b0ef2f28fd83
MichaelSani05 <msmichaelsani@gmail.com>
2025-02-03 13:58:28 +0100

    Responsive house.component y header.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
-import { BookingComponent } from './booking/booking.component';
 import { HouseComponent } from './pages/house/house.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {path: "", component: MainComponent},
+    {path: "alojamiento", component: MainComponent},
     {path: "house/:id", component: HouseComponent},
+    {path: "Cabaña/:id", component: HouseComponent},
+    {path: "Playa/:id", component: HouseComponent},
+    {path: "Mansion/:id", component: HouseComponent},
     {path: "home", component: HomeComponent},
 ];
